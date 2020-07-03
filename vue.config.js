93e283be628bef68aedf5e3caa071ca4c94c1290
jakejoue <jakejoue@live.com>
2020-07-03 14:17:49 +0800

    整合eslint和prettier

---
+++ vue.config.js
@@ -0,0 +1,61 @@
+var webpack = require('webpack');
+
+const isDebug = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    publicPath: './',
+    filenameHashing: isDebug,
+    productionSourceMap: isDebug,
+    lintOnSave: isDebug,
+    // 需要合并的webpack配置
+    configureWebpack: {
+        // 文件打包拆解
+        optimization: {},
+        // 其他插件
+        plugins: [
+            // 定义部分全局变量，打包时候会被替换
+            new webpack.DefinePlugin({
+                env_isDebug: isDebug,
+            }),
+        ],
+    },
+    // 链式修改webpack配置
+    chainWebpack: config => {
+        // cur文件 Loader
+        config.module
+            .rule('cur')
+            .test(/\.cur$/)
+            .use('file-loader')
+            .loader('file-loader')
+            .end();
+    },
+    // 需要babel转义的第三方库
+    transpileDependencies: [/view-design/, /vue-echarts/, /resize-detector/],
+    // 开发用代理服务配置
+    devServer: {
+        port: 9000,
+        proxy: {
+            // 本地
+            '/proxy1': {
+                target: 'http://localhost:8010',
+                // changeOrigin: true,
+                pathRewrite: { '^/proxy1': '' },
+            },
+        },
+    },
+    // 多html配置
+    /*
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html'
+        },
+        map: {
+            entry: 'src/subpages/map.js',
+            template: 'public/map.html',
+            filename: 'map.html'
+        }
+    }
+    */
+};
